bbf7d6bd48f77dec36702193b417fcc9b7bdaed9
Eric Lake <ericallenlake@gmail.com>
2022-09-29 22:25:11 -0600

    Adds correct screenshots & styles component

--- src/data/projects.js
+++ src/data/projects.js
@@ -17,7 +17,7 @@ const projects = [
         "description": "Check out my docs, then fetch my resume as a REST API",
         "stack": ["MongoDB", "Mongoose", "AWS", "Vercel + Serverless Functions", "Node.js", "Express"],
         "__v":0,
-        "url":"/screenshots/habitstack.gif"
+        "url":"/screenshots/api-resume.gif"
     },
     {
         "_id":"6323afd104b45e55b2a26391",
@@ -27,7 +27,7 @@ const projects = [
         "description":"An application to help conscious consumers quickly and easily find the best way to recycle or disposive of common household items",
         "stack":["html","css","bulma","javascript","jquery","socrata open data api","open weather api","geolocation api"],
         "__v":0,
-        "url":"/screenshots/habitstack.gif"
+        "url":"/screenshots/recycle-better.gif"
     },
     {
         "_id":"6323b03804b45e55b2a26394",
@@ -37,17 +37,17 @@ const projects = [
         "description":"A web application using the OpenWeather API to find and store weather information.",
         "stack":["html","css","javascript","openweather api"],
         "__v":0,
-        "url":"/screenshots/habitstack.gif"
+        "url":"/screenshots/screenrecording_weather.gif"
     },
     {
         "_id":"6323b0a004b45e55b2a26397",
         "name":"Portfolio",
         "repo":"https://github.com/yohuck/eric-lake",
         "live":"http://ericallenlake.com",
-        "description":"My portfolio!",
+        "description":"Pretty much the same portfolio, but built in Svelte! (this version is built in React",
         "stack":["svelte","vite","openprops","css grid"],
         "__v":0,
-        "url":"/screenshots/habitstack.gif"
+        "url":"/screenshots/portfolio.PNG"
     }
 
 ]
