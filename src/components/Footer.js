d70df42332e4458f9a491c839166b716ed2c95fd
Eric Lake <ericallenlake@gmail.com>
2022-09-30 21:43:04 -0600

    Completes footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,25 @@
-
 const Footer = () => {
-    return (
-        <div className="footer" id="foot">
-            <h4>Find me online</h4>
-            <ul className="foots">
-                <li>GitHub</li>
-                <li>LinkedIn</li>
-                <li>Twitter</li>
-                <li>CodePen</li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="footer" id="foot">
+      <ul className="foots">
+        <li>
+          <a href="https://github.com/yohuck" target="_blank" rel="noreferrer">
+            <i class="fa-brands fa-github" alt="github"></i>
+          </a>
+        </li>
+        <li>
+          <a href="www.linkedin.com/in/eric-lake" target="_blank" rel="noreferrer">
+            <i class="fa-brands fa-linkedin" alt="linkedin"></i>
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://twitter.com/itsyohuck">
+            <i class="fa-brands fa-twitter" alt="twitter"></i>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
